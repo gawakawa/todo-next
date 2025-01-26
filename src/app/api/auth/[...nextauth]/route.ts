52713e846c766012ba56ba0ae0df9782209fb6ad
gawakawa <hirakuhasegawa@gmail.com>
2025-01-27 00:21:21 +0900

    🔥 Remove unnecessary code

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,5 @@
-import NextAuth, { DefaultSession, NextAuthOptions } from 'next-auth';
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-
- declare module 'next-auth' {
-   interface Session extends DefaultSession {
-     user: {
-       id: string;
-       email: string;
-     } & DefaultSession['user'];
-   }
- }
-
-  declare module 'next-auth/jwt' {
-    interface JWT {
-      id: string;
-      email: string;
-    }
-  }
 
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
@@ -31,30 +15,8 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
 
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user && user?.email) {
-        token.id = user.id;
-        token.email = user.email;
-      }
-      return token;
-    },
-
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.email = token.email;
-      }
-
-      return session;
-    },
-
-    async signIn({ account }) {
-      if (!account || account.provider !== 'google') {
-        return false;
-      }
-
-      return true;
-    },
+  pages: {
+    signIn: '/login',
   },
 };
 
