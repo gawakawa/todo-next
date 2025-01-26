0e2dce29f675eff809d3263e1441e6b46bb82ad1
gawakawa <hirakuhasegawa@gmail.com>
2025-01-27 00:24:18 +0900

    🎨 Format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ const Home = () => {
   const { data, status } = useSession();
 
   useEffect(() => {
-    (status === 'authenticated' && !data.user?.email) && redirect('/register');
-    (status === 'authenticated' && data.user?.email) && redirect('/dashboard');
+    status === 'authenticated' && !data.user?.email && redirect('/register');
+    status === 'authenticated' && data.user?.email && redirect('/dashboard');
     status === 'unauthenticated' && redirect('/login');
   }, [data, status]);
 
